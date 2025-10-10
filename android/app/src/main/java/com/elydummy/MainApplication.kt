f06eea6a41ca9d840e6128e79b8c29e8315d7ee0
prabha-monocept <prabha@monocept.com>
2025-10-10 14:37:28 +0530

    version handling

--- android/app/src/main/java/com/elydummy/MainApplication.kt
+++ android/app/src/main/java/com/elydummy/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.awesomeproject
+package com.elydummy
 
 import android.app.Application
 import com.facebook.react.PackageList
--- index.js
+++ index.js
@@ -5,9 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
-// for library publish change to this code
+// for library
 export default App;
-
-// for independently running the app
+// for debug running
 // AppRegistry.registerComponent(appName, () => App);
