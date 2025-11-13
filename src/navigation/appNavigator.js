69fe031f459465d0ee85d1c0f0e5f16b89c8ef5c
CABPR00889 <chennamadhavuni.avanthika@maxlifeinsurance.com>
2025-11-13 19:16:28 +0530

    ..

--- src/navigation/appNavigator.js
+++ src/navigation/appNavigator.js
@@ -1,26 +1,30 @@
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import { Provider } from "react-redux";
-import store from "../store/store";
-import { ChatPage } from "../components/pages/ChatPage";
-import PropTypes from "prop-types";
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Provider} from 'react-redux';
+import store from '../store/store';
+import {ChatPage} from '../components/pages/ChatPage';
+import PropTypes from 'prop-types';
+// import {NavigationContainer} from '@react-navigation/native';
  
-const AppNavigator = (props) => {
+const AppNavigator = props => {
   const Stack = createStackNavigator();
- 
+
   return (
     <Provider store={store}>
-    <Stack.Navigator>
+{/* -      <NavigationContainer> */}
+        <Stack.Navigator>
           <Stack.Screen
-            name={"header"}
+            name={'header'}
             component={ChatPage}
-            options={{ headerShown: false }}
+            options={{headerShown: false}}
             initialParams={props.props}
           />
         </Stack.Navigator>
+{/* -      </NavigationContainer> */}
     </Provider>
   );
 };
+
  
 AppNavigator.propTypes = {
   props: PropTypes.shape({
